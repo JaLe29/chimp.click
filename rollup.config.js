84e01ee5550385b711cf33415807c357f0eac159
Jakub Löffelmann <jakubloffelmann@gmail.com>
2020-04-19 19:23:20 +0200

    Cleaning, build

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,23 @@
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
-import resolve from "rollup-plugin-node-resolve";
-import typescript from "rollup-plugin-typescript2";
-import sass from "rollup-plugin-sass";
-import commonjs from "rollup-plugin-commonjs";
-import copy from "rollup-plugin-copy";
-import {terser} from "rollup-plugin-terser";
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+import resolve from 'rollup-plugin-node-resolve'
+import typescript from 'rollup-plugin-typescript2'
+import sass from 'rollup-plugin-sass'
+import commonjs from 'rollup-plugin-commonjs'
+import {terser} from 'rollup-plugin-terser'
 
-import packageJson from "./package.json";
+import packageJson from './package.json'
 
 export default {
-	input: "src/index.tsx",
+	input: 'src/index.tsx',
 	output: [
 		{
 			file: packageJson.main,
-			format: "cjs",
+			format: 'cjs',
 			sourcemap: true
 		},
 		{
 			file: packageJson.module,
-			format: "esm",
+			format: 'esm',
 			sourcemap: true
 		}
 	],
@@ -29,35 +28,21 @@ export default {
 		}),
 		typescript({objectHashIgnoreUnknownHack: true}),
 		commonjs({
-			include: ["node_modules/**"],
-			exclude: ["**/*.stories.js"],
+			include: ['node_modules/**'],
+			exclude: ['**/*.stories.js'],
 			namedExports: {
-				"node_modules/react/react.js": [
-					"Children",
-					"Component",
-					"PropTypes",
-					"createElement"
+				'node_modules/react/react.js': [
+					'Children',
+					'Component',
+					'PropTypes',
+					'createElement'
 				],
-				"node_modules/react-dom/index.js": ["render"]
+				'node_modules/react-dom/index.js': ['render']
 			}
 		}),
 		sass({
 			insert: true
 		}),
-		copy({
-			targets: [
-				{
-					src: "src/variables.scss",
-					dest: "build",
-					rename: "variables.scss"
-				},
-				{
-					src: "src/typography.scss",
-					dest: "build",
-					rename: "typography.scss"
-				}
-			]
-		}),
 		terser()
 	]
-};
+}
